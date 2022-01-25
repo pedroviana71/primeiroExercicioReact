ea02c609a174d778b78e743aa49c3aee8fabff06
pedroviana71 <86496233+pedroviana71@users.noreply.github.com>
2022-01-24 21:37:12 -0300

    commit gatito

---
+++ src/olamundo.jsx/olamundo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello gatito marrom!</h1>
+      <img src="..\gatito-marrom.jpg" alt="gatito marrom guloso" />
+    </div>
+  );
+}
+
+export default Hello;
